b62fb5d7a2aa19c14c4258b9fbd8077cdc7e96c7
paulsagar1a <paulsagar1a@gmail.com>
2018-10-28 17:38:07 +0530

    laundry app: second commit

--- laundryfitv1/src/main/java/com/laundryfit/dao/HibernateCfg.java
+++ laundryfitv1/src/main/java/com/laundryfit/dao/HibernateCfg.java
@@ -6,9 +6,9 @@ class HibernateCfg {
 		public static Configuration configuration = new Configuration()
 				.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
 			    .setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
-			    .setProperty("hibernate.connection.url", "jdbc:mysql://localhost/laundryorder?useSSL=false")
-			    .setProperty("hibernate.connection.username", "root")
-			    .setProperty("hibernate.connection.password", "")
+			    .setProperty("hibernate.connection.url", "**********")
+			    .setProperty("hibernate.connection.username", "**********")
+			    .setProperty("hibernate.connection.password", "**********")
 			    .setProperty("hibernate.show_sql", "true")
 			    .setProperty("hibernate.hbm2ddl.auto", "update");
 }
